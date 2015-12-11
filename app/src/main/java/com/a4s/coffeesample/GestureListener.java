8168376f81f2c3649293e1cad7b8ed75351bb228
Donncha Finlay <dfinlay.externe@pagesjaunes.fr>
2015-12-11 17:45:25 +0100

    Update For messages

---
+++ app/src/main/java/com/a4s/coffeesample/GestureListener.java
@@ -0,0 +1,20 @@
+/*
+ * Created on 08/12/15, 17:36
+ *
+ * Copyright(c) 2015 PagesJaunes, SoLocal Group - All Rights Reserved.
+ * Unauthorized copying of this file, via any medium is strictly prohibited. Proprietary and confidential
+ *
+ */
+package com.a4s.coffeesample;
+
+/**
+ * //TODO : Add a class header comments
+ * <p>
+ * created on 08/12/15
+ *
+ * @author donnchafinlaypj
+ * @version //TODO : add version
+ */
+public class GestureListener
+{
+}
